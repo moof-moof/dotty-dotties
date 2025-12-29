82b7a98c158042c6b3b437d08a24a0a3ff19f296
moof-moof <bergman.martin@gmail.com>
2025-12-29 03:27:09 +0100

    Correction of .xsession-error cleanser-script

--- ~\bin/sh/xsession-errors_nullifier.sh
+++ ~\bin/sh/xsession-errors_nullifier.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 echo "Quietly nullifying the ~/.xsession-errors file: [STARTS SCRIPT]"
-echo "(Sleeping for 1 minute to make sure the spamming has commenced ...)"
 
-rm -f ~/.xsession-errors ; rm -f ~/.xsession-errors.old ; ln -s /dev/null ~/.xsession-errors
+echo "(Sleeping for 30 secs to make sure the spamming has commenced ...)"
+
+/bin/sleep 30
+
+if [ ~/.xsession-errors.old ]; then
+    rm -f ~/.xsession-errors.old ;
+fi;
+
+rm -f ~/.xsession-errors;  ln -s /dev/null ~/.xsession-errors
 
 echo "There, done nullifying for now: [STOPS SCRIPT]"
